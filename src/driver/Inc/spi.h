fbd5c2289c55474cacd4ec68e779c19a1df3a70d
Matthias Nigmann <7ta5j2nq@duck.com>
2024-11-15 10:49:17 -0500

    Add missing declarations for SPI start and stop

--- src/driver/Inc/spi.h
+++ src/driver/Inc/spi.h
@@ -8,5 +8,7 @@
 
 bool core_SPI_init(SPI_TypeDef *spi, GPIO_TypeDef *cs_port, uint16_t cs_pin);
 bool core_SPI_read_write(SPI_TypeDef *spi, uint8_t *txbuf, uint32_t txbuflen, uint8_t *rxbuf, uint32_t rxbuflen);
+bool core_SPI_start(SPI_TypeDef *spi);
+bool core_SPI_stop(SPI_TypeDef *spi);
 
 #endif
