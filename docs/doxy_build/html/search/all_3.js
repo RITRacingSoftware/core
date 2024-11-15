1f2183abb6386cfb9daee35a11b545ca9c40d789
Sls <Solomonshulman13@gmail.com>
2024-11-15 14:25:20 -0500

    Reformatted documentation, added Sphinx
    Put documentation files in one docs subdirectory. Added Sphinx implementation so ReadTheDocs can be used to host.

---
+++ docs/doxy_build/html/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['enabling_20and_20disabling_0',['Enabling and disabling',['../usart_8c.html#autotoc_md8',1,'']]]
+];
---
+++ docs/doxy_build/html/search/files_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['can_2ec_0',['can.c',['../can_8c.html',1,'']]],
+  ['clock_2ec_1',['clock.c',['../clock_8c.html',1,'']]]
+];
