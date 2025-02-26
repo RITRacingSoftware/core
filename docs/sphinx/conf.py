71bb8b80ce4270c22870444296088a306eb36133
Sls <Solomonshulman13@gmail.com>
2025-02-26 11:23:15 -0500

    Changed documentation

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -6,10 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import shutil
 import subprocess
-
-subprocess.call('cd ..; doxygen', shell = True)
 
 project = 'Core'
 copyright = '2024, RIT-Formula'
@@ -27,5 +24,3 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 html_static_path = ['_static']
 html_extra_path = ['../doxy_build/html']
-
-# shutil.copyfile('../doxy_build/html/index.html', './_build/html/index.html')
