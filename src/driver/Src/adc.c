64769f3947d60d0fdb7ebd71248b75e40bcd2481
Matthias Nigmann <7ta5j2nq@duck.com>
2025-02-26 18:37:32 -0500

    Add ADC pin table

--- src/driver/Src/adc.c
+++ src/driver/Src/adc.c
@@ -24,6 +24,59 @@
   * To read from an analog input, the user code calls core_ADC_read_channel().
   * The result is stored in a pointer passed as an argument, and the return
   * value specifies whether the conversion was successful or not.
+  *
+  * ## Table of connections
+  * The table below indicates which pins connect to which ADC and whether an
+  * op amp can be connected in between the input and the ADC. If a table cell
+  * contains a number, then the pin corresponding to the row connects to the
+  * ADC corrsponding to the column directly. If the cell also indicates an 
+  * op amp (OPAMP1-OPAMP6), then the pin can be connected to the ADC via
+  * an op amp. Note that not all chips will have all 5 ADCs. 
+  * | Pin  |    ADC1    |    ADC2    |    ADC3    |    ADC4    |    ADC5    |
+  * | ---- | ----------:| ----------:| ----------:| ----------:| ----------:|
+  * | PA0  |          1 |          1 |            |            |            |
+  * | PA1  |   OPAMP1/2 |   OPAMP3/2 |     OPAMP3 |            |            |
+  * | PA2  |          3 |            |            |            |            |
+  * | PA3  |   OPAMP1/4 |            |            |            |            |
+  * | PA4  |            |         17 |            |            |            |
+  * | PA5  |            |         13 |            |            |            |
+  * | PA6  |            |          3 |            |            |            |
+  * | PA7  |     OPAMP1 |   OPAMP2/4 |            |            |            |
+  * | PA8  |            |            |            |            |          1 |
+  * | PA9  |            |            |            |            |          2 |
+  * | PB0  |         15 | OPAMP2/OPAMP3 | OPAMP3/12 |          |            |
+  * | PB1  |         12 |            |          1 |            |            |
+  * | PB2  |            |         12 |            |            |            |
+  * | PB11 |         14 |         14 |            |            |     OPAMP4 |
+  * | PB12 |         11 |            |            |   OPAMP6/3 |            |
+  * | PB13 |            |     OPAMP3 |   OPAMP3/5 |     OPAMP6 |     OPAMP4 |
+  * | PB14 |          5 |     OPAMP2 |            |          4 |     OPAMP5 |
+  * | PB15 |            |         15 |            |          5 |            |
+  * | PC0  |          6 |          6 |            |            |            |
+  * | PC1  |          7 |          7 |            |            |            |
+  * | PC2  |          8 |          8 |            |            |            |
+  * | PC3  |          9 |          9 |            |            |     OPAMP5 |
+  * | PC4  |            |          5 |            |            |            |
+  * | PC5  |            |         11 |            |            |            |
+  * | PD10 |            |            |          7 |          7 |          7 |
+  * | PD11 |            |            |          8 |          8 |   OPAMP4/8 |
+  * | PD12 |            |            |          9 |          9 |   OPAMP5/9 |
+  * | PD13 |            |            |         10 |         10 |         10 |
+  * | PD14 |            |    OPAMP2  |         11 |         11 |         11 |
+  * | PD8  |            |            |            |         12 |         12 |
+  * | PD9  |            |            |            |  OPAMP6/13 |         13 |
+  * | PE7  |            |            |          4 |            |            |
+  * | PE8  |            |            |          6 |          6 |          6 |
+  * | PE9  |            |            |          2 |            |            |
+  * | PE10 |            |            |         14 |         14 |         14 |
+  * | PE11 |            |            |         15 |         15 |         15 |
+  * | PE12 |            |            |         16 |         16 |         16 |
+  * | PE13 |            |            |          3 |            |            |
+  * | PE14 |            |            |            |          1 |            |
+  * | PE15 |            |            |            |          2 |            |
+  * | PF0  |         10 |            |            |            |            |
+  * | PF1  |            |         10 |            |            |            |
+  * 
   */
 
 #include "adc.h"
