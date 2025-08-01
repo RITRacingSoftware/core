bb77b9c6829d75f71a85426ebc951e377769e00c
Matthias Nigmann <7ta5j2nq@duck.com>
2025-08-01 16:51:03 +0200

    Fix GPIOE clock init

--- src/driver/Src/clock.c
+++ src/driver/Src/clock.c
@@ -154,6 +154,7 @@ void core_clock_port_init(GPIO_TypeDef *port) {
     else if (port == GPIOB) __HAL_RCC_GPIOB_CLK_ENABLE();
     else if (port == GPIOC) __HAL_RCC_GPIOC_CLK_ENABLE();
     else if (port == GPIOD) __HAL_RCC_GPIOD_CLK_ENABLE();
+    else if (port == GPIOE) __HAL_RCC_GPIOE_CLK_ENABLE();
     else if (port == GPIOF) __HAL_RCC_GPIOF_CLK_ENABLE();
     else if (port == GPIOG) __HAL_RCC_GPIOG_CLK_ENABLE();
 }
