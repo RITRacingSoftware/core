8d71c734daa07b8a27e5a8fd261a10caec962448
Matthias Nigmann <7ta5j2nq@duck.com>
2025-10-16 23:52:16 -0400

    Fix MCan scripts

--- scripts/mcan_vc.sh
+++ scripts/mcan_vc.sh
@@ -1,6 +1,8 @@
-python3 scripts/program_can.py txoff
+#!/usr/bin/bash
+
+python3 scripts/program_can.py txoff_retry
 python3 scripts/program_can.py c70off
-python3 scripts/program_can.py boot 1
-python3 scripts/program_can.py program 4 ../core-vc/build/stm32/core-vc-f33.ihex
+#python3 scripts/program_can.py boot 1
+python3 scripts/program_can.py program 4 ../core-vc/build/stm32/core-vc-f34.ihex
 python3 scripts/program_can.py c70on
 python3 scripts/program_can.py txon
